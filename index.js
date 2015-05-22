b3024a164afb373c8f1323cf125a7765c9285f12
Vu Tran <baysao@gmail.com>
2015-05-22 10:53:31 +0700

    support multi level file structure

--- index.js
+++ index.js
@@ -1,99 +1,108 @@
 'use strict';
 
 var fs = require('fs'),
-	path = require('path'),
-	extend = require('extend');
-
+    path = require('path'),
+    _ = require('lodash'),
+    extend = require('extend');
+//var jf = require("jsonfile");
 // return true if path is directory
 var isDir = function (dir) {
-	return fs.existsSync(dir) ? fs.lstatSync( dir ).isDirectory() : false;
+    return fs.existsSync(dir) ? fs.lstatSync(dir).isDirectory() : false;
 };
 
 // return path of folder with same name as .json file if exists
 var getNamesake = function (file) {
-	var folder = path.dirname(file) + '/' + path.basename( file, '.json' );
-	return fs.existsSync(folder) && isDir(folder) ? folder : false;
+    var folder = path.dirname(file) + '/' + path.basename(file, '.json');
+    return fs.existsSync(folder) && isDir(folder) ? folder : false;
 };
 
 // get the paths of files and folders inside a folder
 var getFolderElements = function (parent) {
-	var elems = fs.readdirSync( parent ),
-		d = {
-			files : [],
-			folders : []
-		};
+    var elems = fs.readdirSync(parent),
+        d = {
+            files: [],
+            folders: []
+        };
 
-	// get files and folders paths
-	elems.forEach( function (elem) {
-		var el = parent + '/' + elem;
-		if (!isDir( el )) {
-			d.files.push( el );
-		} else if (!fs.existsSync( el + '.json')) {
-			// add folder only if has no namesake json
-			d.folders.push( el );
-		}
-	});
+    // get files and folders paths
+    elems.forEach(function (elem) {
+        var el = parent + '/' + elem;
+        if (!isDir(el)) {
+            d.files.push(el);
+        } else if (!fs.existsSync(el + '.json')) {
+            // add folder only if has no namesake json
+            d.folders.push(el);
+        }
+    });
 
-	return d;
+    return d;
 };
 
 
 var getData = {
 
-	// get file data and extend it with folder data of folder with same name
-	file : function (file) {
-		var config = require( file ),
-			namesake = getNamesake( file );
+    // get file data and extend it with folder data of folder with same name
+    file: function (file) {
+        console.log(file);
+        if(fs.existsSync(file)) {
+            console.log(file + ' exists');
+        }
+        //var config = fs.existsSync(file)?jf.readFileSync(file):{},
+        var config = fs.existsSync(file)?require(file):{},
+            namesake = getNamesake(file);
+        var returnValue = config;
+        if (namesake) {
+            if (Array.isArray(config)) {
+                returnValue = _.union(config, _.values(getData.folder(namesake)))
+            } else
+                returnValue = extend(config, getData.folder(namesake))
+        }
+        return returnValue;
+    },
 
-		return namesake ? extend( config, getData.folder( namesake )) : config;
-	},
+    // get data from folders and files inside a folder
+    folder: function (folder) {
+        var elems = getFolderElements(folder);
+        var result = {};
 
-	// get data from folders and files inside a folder
-	folder : function (folder) {
-		var elems = getFolderElements( folder ),
-			result = {};
+        // files
+        elems.files.forEach(function (route) {
+            // get object name
+            var fileName = path.basename(route, '.json');
+            // assign object data from file
+            result[fileName] = getData.file(route);
+        });
 
-		// files
-		elems.files.forEach( function (route) {
-			// get object name
-			var fileName = path.basename( route, '.json' );
-			// assign object data from file
-			result[ fileName ] = getData.file( route );
-		});
-
-		// no namesake folders
-		elems.folders.forEach( function (route) {
-			// get object name
-			var fileName = path.basename( route );
-			// assign data from folder
-			result[ fileName ] = extend( result[ fileName ] || {}, getData.folder( route ));
-		});
-
-		return result;
-	}
+        // no namesake folders
+        elems.folders.forEach(function (route) {
+            // get object name
+            var fileName = path.basename(route);
+            // assign data from folder
+            result[fileName] = extend(result[fileName] || {}, getData.folder(route));
+        });
+        //if(isArray) {
+        //    return _.values(result);
+        //}
+        return result;
+    }
 };
 
 
 module.exports = function () {
-	var configs = [],
-		i;
+    var configs = [],
+        i;
 
-	if (!arguments.length) {
-		return {};
-	}
+    if (!arguments.length) {
+        return {};
+    }
 
-	for (i in arguments) {
-		if (typeof arguments[i] !== 'string') {
-			if (typeof arguments[i] !== 'object' || typeof arguments[i].length !== 'undefined' || arguments[i] === null) {
-				throw new Error( 'deep-json: bad file argument' );
-			} else {
-				configs[i] = arguments[i];
-			}
-		} else {
-			configs[i] = getData.file( path.resolve( arguments[i] ));
-		}
+    for (i in arguments) {
+        if (typeof arguments[i] !== 'string') {
+            throw new Error('deep-json: bad file argument');
+        }
+        console.log(path.resolve(arguments[i]));
+        configs[i] = getData.file(path.resolve(arguments[i]));
+    }
 
-	}
-
-	return extend.apply( {}, configs );
+    return extend.apply({}, configs);
 };
